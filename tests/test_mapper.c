c4609b065098461db67def3b3a7f8f64ecfd4d04
Olivier Detour <olivier.detour@codecasesoftware.com>
2016-04-19 18:26:52 +0200

    improve tests.

--- tests/test_mapper.c
+++ tests/test_mapper.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include <counter.h>
 #include <mapper.h>
 
-void c_print(void const * a, void * data)
+void c_init(void const * a, void * data)
 {
-	FILE * s = (FILE *)data;
-
-	counter_print((counter_t const *)a, s);
-	fprintf(s, " ");
+	counter_t * c = (counter_t *)a;
+	unsigned int * rv = (unsigned int *)data;
+	c->value = *rv;
 }
 
-void * doit(void * data)
+void * r_init(void * data)
 {
 	runner_t * r = (runner_t *)data;
 
-	fprintf(stdout, "[%d]", r->index);
-	list_node_foreach(r->list, c_print, stdout);
-	fprintf(stdout, "\n");
+	list_node_foreach(r->list, c_init, &r->index);
 
 	pthread_exit(data);
 }
@@ -30,10 +28,24 @@ int main(int argc,
 	if (argc < 3)
 		return 1;
 
-	unsigned int	len = atoi(argv[1]);
+	unsigned int	n = atoi(argv[1]);
 	mapper_t *		mapper;
 
-	mapper = mapper_new(len, argv + 2);
-	mapper_run(mapper, doit);
+	mapper = mapper_new(n, argv + 2);
+	mapper_run(mapper, r_init);
+
+	for (unsigned int i = 0; i < mapper->n; ++i)
+	{
+		list_node_t * p = mapper->runners[i].list;
+
+		while (p)
+		{
+			counter_t * c = (counter_t *)p->elt;
+
+			assert(c->value == i);
+			p = p->next;
+		}
+	}
+
 	mapper_free(mapper);
 }
