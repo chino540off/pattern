e1ebce531693be6e4a9b30346ded1eedf4581976
Olivier Detour <olivier.detour@codecasesoftware.com>
2016-04-19 17:24:50 +0200

    cast instead of new method

--- src/mapper.c
+++ src/mapper.c
@@ -9,16 +9,6 @@ int c_cmp(void const * a, void const * b)
 	return strcmp(((counter_t const *)a)->pattern, ((counter_t const *)b)->pattern);
 }
 
-void * c_dup(void const * elt)
-{
-	return counter_dup((counter_t const *)elt);
-}
-
-void c_free(void * elt)
-{
-	counter_free((counter_t *)elt);
-}
-
 mapper_t * mapper_new(unsigned int n, char const * argv[])
 {
 	mapper_t * mapper = 0;
@@ -38,7 +28,7 @@ mapper_t * mapper_new(unsigned int n, char const * argv[])
 		mapper->arr[0] = list_node_insert(mapper->arr[0], counter_new(0, argv[i]), c_cmp);
 
 	for (unsigned int i = 1; i < n; ++i)
-		mapper->arr[i] = list_node_dup(mapper->arr[0], c_dup);
+		mapper->arr[i] = list_node_dup(mapper->arr[0], (list_t_dup_f)counter_dup);
 
 	return mapper;
 }
@@ -46,7 +36,7 @@ mapper_t * mapper_new(unsigned int n, char const * argv[])
 void mapper_free(mapper_t * mapper)
 {
 	for (unsigned int i = 0; i < mapper->n; ++i)
-		list_node_free(mapper->arr[i], c_free);
+		list_node_free(mapper->arr[i], (list_t_free_f)counter_free);
 
 	free(mapper->arr);
 	free(mapper);
