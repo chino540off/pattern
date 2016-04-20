28a9ec061ec0d21dd712f643db47ac5945ac1b5a
Olivier Detour <olivier.detour@codecasesoftware.com>
2016-04-20 16:29:57 +0200

    Add reducing into pattern file directly, have to fix, juste for tests.

--- src/pattern.c
+++ src/pattern.c
@@ -5,6 +5,24 @@
 #include <mapper.h>
 #include <pattern.h>
 
+static void reduce(void const * elt, void * data)
+{
+	list_node_t ** p = (list_node_t **)data;
+	counter_t * c_cur = (counter_t *)elt;
+	counter_t * c_res = (counter_t *)(*p)->elt;
+
+	c_res->value += c_cur->value;
+
+	*p = (*p)->next;
+}
+
+static void print(void const * elt, void * data)
+{
+	counter_t * counter = (counter_t *)elt;
+	counter_print(counter, stdout);
+	fprintf(stdout, "\n");
+}
+
 int main(int argc, char const * argv[])
 {
 	if (argc < 4)
@@ -28,7 +46,10 @@ int main(int argc, char const * argv[])
 		contexts[i].filename = filename;
 
 		for (unsigned int j = 3; argv[j]; ++j)
-			contexts[i].list = list_node_insert(contexts[i].list, counter_new(0, argv[j]), (list_t_cmp_f)counter_cmp);
+		{
+			counter_t * c = counter_new(0, argv[j]);
+			contexts[i].list = list_node_insert(contexts[i].list, c, (list_t_cmp_f)counter_cmp);
+		}
 
 		pcontexts[i] = &contexts[i];
 	}
@@ -37,8 +58,25 @@ int main(int argc, char const * argv[])
 	mapper_run(mapper, pattern_map);
 	mapper_free(mapper);
 
+	list_node_t * result = 0;
+
+	for (unsigned int j = 3; argv[j]; ++j)
+	{
+		counter_t * c = counter_new(0, argv[j]);
+		result = list_node_insert(result, c, (list_t_cmp_f)counter_cmp);
+	}
+
 	for (unsigned int i = 0; i < n; ++i)
+	{
+		list_node_t * p = result;
+		list_node_foreach(contexts[i].list, reduce, &p);
+
 		list_node_free(contexts[i].list, (list_t_free_f)counter_free);
+	}
+
+	list_node_foreach(result, print, 0);
+
+	list_node_free(result, (list_t_free_f)counter_free);
 
 	return 0;
 }
