2cdaf21fb04f67949ae4d8c48a47dfc917728dd1
Olivier Detour <olivier.detour@codecasesoftware.com>
2016-04-20 10:28:42 +0200

    use in pattern binary the new mapper api.

--- src/pattern.c
+++ src/pattern.c
@@ -1,4 +1,60 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include <counter.h>
+#include <mapper.h>
+
+static int c_cmp(void const * a, void const * b)
+{
+	return strcmp(((counter_t const *)a)->pattern, ((counter_t const *)b)->pattern);
+}
+
+struct counter_ctx_s
+{
+	list_node_t * list;
+	char const * filename;
+};
+typedef struct counter_ctx_s counter_ctx_t;
+
+static void * do_count(void * data)
+{
+	runner_t * r = (runner_t *)data;
+	counter_ctx_t * ctx = (counter_ctx_t *)r->data;
+
+	FILE * fd = 0;
+
+	if ((fd = fopen(ctx->filename, "r")) == 0)
+	{
+		perror("fopen: ");
+		pthread_exit(data);
+	}
+}
+
 int main(int argc, char const * argv[])
 {
+	if (argc < 4)
+		return 1;
+
+	char const *	filename = argv[1];
+	unsigned int	n = atoi(argv[2]);
+	mapper_t *		mapper;
+	counter_ctx_t	contexts[n];
+	void *			pcontexts[n];
+
+	for (unsigned int i = 0; i < n; ++i)
+	{
+		contexts[i].list = 0;
+		contexts[i].filename = filename;
+
+		for (unsigned int j = 3; argv[j]; ++j)
+			contexts[i].list = list_node_insert(contexts[i].list, counter_new(0, argv[j]), c_cmp);
+
+		pcontexts[i] = &contexts[i];
+	}
+
+	mapper = mapper_new(n, pcontexts);
+	mapper_run(mapper, do_count);
+	mapper_free(mapper);
+
 	return 0;
 }
