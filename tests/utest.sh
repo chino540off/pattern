2a3afd2e518b933c1e7d0ebcf923a5aaab7171f1
Olivier Detour <olivier.detour@codecasesoftware.com>
2016-04-21 11:39:36 +0200

    improve pattern tests: remove file if test succes.

--- tests/utest.sh
+++ tests/utest.sh
@@ -3,7 +3,8 @@ dir_tests=$2
 no_test=$3
 nt=$4
 
-$bin $dir_tests/refs/${no_test}.txt 1 `cat $dir_tests/refs/${no_test}.args` > /tmp/pattern_${no_test}_${nt}.ref || exit 1
-$bin $dir_tests/refs/${no_test}.txt $nt `cat $dir_tests/refs/${no_test}.args` > /tmp/pattern_${no_test}_${nt}.test || exit 1
+$bin $dir_tests/refs/${no_test}.txt 1 `cat $dir_tests/refs/${no_test}.args` > /tmp/test_pattern_${no_test}_${nt}.ref || exit 1
+$bin $dir_tests/refs/${no_test}.txt $nt `cat $dir_tests/refs/${no_test}.args` > /tmp/test_pattern_${no_test}_${nt}.test || exit 1
 
-diff /tmp/pattern_${no_test}_${nt}.ref /tmp/pattern_${no_test}_${nt}.test
+diff /tmp/test_pattern_${no_test}_${nt}.ref /tmp/test_pattern_${no_test}_${nt}.test || exit 1
+rm /tmp/test_pattern_${no_test}_${nt}.ref /tmp/test_pattern_${no_test}_${nt}.test || exit 1
