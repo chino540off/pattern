75a1833aa8174332f51e35609789d650288a1aa1
Olivier Detour <olivier.detour@codecasesoftware.com>
2016-04-29 10:40:53 +0200

    add lcov handling and add hashmap test into ctest.

--- build.sh
+++ build.sh
@@ -8,4 +8,11 @@ mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
 make
-cp src/pattern ..
+
+lcov --zerocounters --directory .
+lcov --capture --initial --directory . --output-file app
+
+ctest
+
+lcov --no-checksum --directory . --capture --output-file app.info
+genhtml app.info -o genhtml
